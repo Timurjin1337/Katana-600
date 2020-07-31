45702863f4d4651ca2a86513bdd3256e819e81f7
Timurjin1337 <68728498+Timurjin1337@users.noreply.github.com>
2020-07-31 11:59:29 +0200

    Add files via upload

---
+++ test1.py
@@ -0,0 +1,35 @@
+# int (integer) - целое число
+number = 5
+age = 36
+
+# float - дробные числа
+fnumber = 5.7
+
+# str (string) все слова
+name = "Dimon"
+
+# bool   True or False
+status = True
+
+#print("xyz")
+print("Test text string: Yo Dimon!")
+#print(name)
+#print(fnumber)
+
+# ekranirovanije - \"
+print("Dimon - \"rediso4nqj\" chelovek!")
+# mozhno ispol'zovat' vydelenije bez ekranirovanija "a's'd" ili 'a"s"d'
+
+# Perevod stroki \n
+print("Privet \nmir")
+
+# concatenation +
+print("Privet, okun' - " + name + "!")
+# konkatenacija string i float ili integer osushestvlajetsja perevodom tipov dannqh v "str()"
+print("Mne " + str(age) + " let!")
+
+name = input("Input name: ")
+age = input("Your age: ")
+
+print("Privet, " + name + "!")
+print("Ogo, tebe uzhe " + age + " goda!")
---
+++ test2.py
@@ -0,0 +1,1 @@
+# +, -, *, /, **, unarnqj minus, okruglenije, Pi
